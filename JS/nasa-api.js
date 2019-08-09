fb356baca38fce0dea07d3503cab850af5aaa639
Jian <jianxie6464@gmail.com>
2019-08-09 14:43:39 -0700

    Ajax in the click function

--- JS/nasa-api.js
+++ JS/nasa-api.js
@@ -1,4 +1,4 @@
-
+/* global $ */
 /*let searchInput = document.getElementById("searchInput")
 let searchButton = document.getElementById("searchButton")
 let searchResult = document.getElementById("searchResult")
@@ -10,21 +10,21 @@ function inputClick(){
     }
 }*/
 let url =  "https://api.nasa.gov/planetary/apod?api_key=zbSs6K5WiNMfDe3PWJb2m4RV92LGgykgYGyaTwl1";
-
+let inputVal;
 $(document).ready(function(){
-    function getAPI(){ 
-        $.ajax({
-            url:url,
-            method:"GET",
-            success: function(){
-                
-            }
-        })
-
-    }
-
     $("#searchButton").click(function(){
-        
+        function getAPI(){ 
+            inputVal = $("#searchInput").val();
+            console.log(inputVal)
+            $.ajax({
+                url:url,
+                method:"GET",
+                success: function(){
+                
+                }
+            })
+
+        } 
     })
 
 })
